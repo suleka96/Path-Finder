87a5c8e43ab50f6b320dac93f487086df74f455b
suleka96 <sulekahelmini96comp@gmail.com>
2018-04-11 01:26:05 +0530

    grid creation done

--- src/GUI.java
+++ src/GUI.java
@@ -1,9 +1,15 @@
+
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.*;
-import javafx.stage.Stage;
+        import javafx.geometry.Insets;
+        import javafx.geometry.Pos;
+        import javafx.scene.Scene;
+        import javafx.scene.control.Button;
+        import javafx.scene.layout.*;
+        import javafx.scene.paint.Color;
+        import javafx.scene.shape.Rectangle;
+        import javafx.stage.Stage;
+
+        import java.util.Random;
 
 public class GUI extends Application {
 
@@ -15,50 +21,80 @@ public class GUI extends Application {
     public void start(Stage primaryStage) {
 
         BorderPane mainLayer = new BorderPane();
+
+//        mapGrid.setHgap(20);//setting horizontal gap between grid lines
+//        mapGrid.setVgap(20);// setting vertical gap between lines
         mainLayer.setPadding(new Insets(0, 0, 0, 0));
 
-            GridPane mapGrid = new GridPane();
-
+        GridPane mapGrid = new GridPane();
         mapGrid.setStyle("-fx-border-color: #3399CC;");//setting background color of the grid
-        mapGrid.setHgap(20);//setting horizontal gap between grid lines
-        mapGrid.setVgap(20);// setting vertical gap between lines
-        mapGrid.setPadding(new Insets(0, 0, 0, 0));//setting padding in the grid cell
-        mapGrid.setPrefSize(100,100);
 
         //creating column constraints for 10 columns
         for (int i = 0; i < 20; i++) {
             ColumnConstraints column = new ColumnConstraints();
 //            column.setPercentWidth(100/20);//setting width of each column
+            column.setFillWidth(true);
+            column.setHgrow(Priority.ALWAYS);
             mapGrid.getColumnConstraints().add(column);
-
-
-
         }
         //creating row constraints for 20 rows
         for (int i = 0; i < 20; i++) {
             RowConstraints row = new RowConstraints();
+            row.setFillHeight(true);
+            row.setVgrow(Priority.ALWAYS);
 //            row.setPercentHeight(100/20);//setting height of each row
+
             mapGrid.getRowConstraints().add(row);
         }
 
-            mainLayer.setCenter(mapGrid);
-            mapGrid.setGridLinesVisible(true);
+        Random rand = new Random();
+        Color[] colors = {Color.BLACK, Color.BLUE, Color.GREEN, Color.RED};
 
-            HBox controls = new HBox();
-            controls.setPrefSize(1300,150);
+        int n = rand.nextInt(4)+1;
 
-            HBox bottom = new HBox();
-            bottom.setPrefSize(1300,300);
+        for (int r = 0; r < 20; r++) {
+            for (int c = 0; c < 20; c++) {
+
+
+                int num = rand.nextInt(4);
+                Rectangle rec = new Rectangle();
+                rec.setWidth(19);
+                rec.setHeight(19);
+                rec.setFill(colors[num]);
+                GridPane.setRowIndex(rec, r);
+                GridPane.setColumnIndex(rec, c);
+                mapGrid.getChildren().addAll(rec);
+
+            }
+        }
+
+        mainLayer.setAlignment(mapGrid, Pos.CENTER);
+        mainLayer.setMargin(mapGrid, new Insets(38,210,38,210));
+        mainLayer.setCenter(mapGrid);
+
+
+//        mapGrid.setGridLinesVisible(true);
+
+        HBox controls = new HBox();
+        controls.setPrefSize(1300,150);
+
+        HBox bottom = new HBox();
+        bottom.setPrefSize(1300,300);
 
         bottom.setStyle("-fx-border-color: #3399CC;");//setting background color of the grid
 
-            controls.setStyle("-fx-border-color: #3399CC;");//setting background color of the grid
+        controls.setStyle("-fx-border-color: #3399CC;");//setting background color of the grid
 
-            mainLayer.setTop(controls);
+        mainLayer.setTop(controls);
 
-            mainLayer.setBottom(bottom);
+        mainLayer.setBottom(bottom);
 
-            mainLayer.setPrefSize(1300,600);
+        mainLayer.setPrefSize(1300,600);
+
+
+
+
+
 //
 
 
